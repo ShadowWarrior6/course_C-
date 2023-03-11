feaade7c8b4285553188f6d416d603514973a325
Andranik <andran1401@gmail.com>
2023-03-11 20:22:17 +0400

    Квадраты

---
+++ lesson_1/1_0/1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿string s_a = Console.ReadLine()!;
+string s_b = Console.ReadLine()!;
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+
+if (a == b * b) Console.WriteLine("yes");
+else Console.WriteLine("No");
+
