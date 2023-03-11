f2e77feec780f1458760bfe55b606584f6f52db2
Andranik <andran1401@gmail.com>
2023-03-11 20:41:39 +0400

    Week

---
+++ lesson_1/1_0/1_1/1_2/Program.cs
@@ -0,0 +1,35 @@
+﻿string s_a= Console.ReadLine()!;
+
+if (s_a == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+else if (s_a == "2")
+{
+    Console.WriteLine("вторник");
+}
+
+else if (s_a == "3")
+{
+    Console.WriteLine("ср");
+}
+else if (s_a == "4")
+{
+    Console.WriteLine("чт");
+}
+else if (s_a == "5")
+{
+    Console.WriteLine("пят");
+}
+else if (s_a == "6")
+{
+    Console.WriteLine("Су");
+}
+else if (s_a == "7")
+{
+    Console.WriteLine("вс");
+}
+else
+{
+    Console.WriteLine("Неопознаный день недели");
+} 
